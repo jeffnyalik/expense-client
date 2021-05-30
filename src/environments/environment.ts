ebed809a7c9e255249363fbce3e4c91ce40b7f2f
Jeff Nyalik <jeffnyak@gmail.com>
2021-05-30 14:55:31 +0300

    added production endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'http://localhost:8000/api/'
+  // BASE_URL: 'http://localhost:8000/api/'
+  // BASE_URL: 'http://127.0.0.1:8000/api/'; //local endpoint 
+  BASE_URL: 'https://expenseapp1990.herokuapp.com/api/' //production endpoint
 };
 
 /*
